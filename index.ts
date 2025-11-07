620c6e3ab7b7b93ae3b8aa12c4360299c26a51e0
oovaa <omarvx211@gmail.com>
2025-11-07 15:47:56 +0200

    using redis client from start  at 2025-11-07

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import { redis, RedisClient } from 'bun'
 
 // Using the default client (reads connection info from environment)
 // process.env.REDIS_URL is used by default
-await redis.set('hello', 'world')
-const result = await redis.get('hello')
 
 // Creating a custom client
 const client = new RedisClient('redis://localhost:6370')
+
+await client.set('hello', 'world')
+const result = await client.get('hello')
 await client.set('counter', '0')
 await client.incr('counter')
 
